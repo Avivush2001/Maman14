02f4ceebfb0839d6cb96500380bed866b7b9216f
Oded Anuka <oded6anuka@gmail.com>
2024-03-12 20:51:32 +0200

    c

--- stageOne.c
+++ stageOne.c
@@ -362,7 +362,13 @@ OperandsFlags getOperandType(char *token)
 
     else if(isLabelLegal(token) == True)
         flag = isLabel;
-        
-    else if()
 
+    else if(isLegalArray(token) == True)
+        flag = isArray;
+
+    else if(findInStringArray(token, registersArr, 8) != -1)
+        flag = isRegister;
+    else
+        flag = illegalOperand;
+    return flag;
 }
